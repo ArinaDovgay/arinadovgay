5d4ad55c3f5231cd35c14ba6030176326bfdfdcc
ArinaDovgay <95289087+ArinaDovgay@users.noreply.github.com>
2022-08-24 08:13:45 +0000

    Deploying to gh-pages from @ ArinaDovgay/arinadovgay@1a3da8e3a9a989071bea5921fc95e7a8eb1e448f 🚀

---
+++ _next/static/chunks/d64684d8-2ddc0d2342ab5bb7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{3854:function(c,b,a){a.d(b,{Ba7:function(){return e}});var d=a(8357);function e(a){return(0,d.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M5 11l7-7 7 7M5 19l7-7 7 7"}}]})(a)}}}])
