6e11bc0d491eff83f8c6e5caf1ef8cfab21a05f6
ArinaDovgay <arinadovgay@gmail.com>
2022-08-24 14:19:02 +0300

    4.

--- components/About.jsx
+++ components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import AboutImg from "../public/assets/photo.jpg";
+import EnvArina from "../.env";
 
 const About = () => {
   return (
@@ -44,7 +45,7 @@ const About = () => {
           </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image src={AboutImg} className="rounded-xl" alt="/" />
+          <Image src={`${process.env.NEXT_PUBLIC_ARINA}/public/assets/photo.jpg`} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
