c0bd8b4bfbd0498c25c710caa6c694c240863c5f
ArinaDovgay <arinadovgay@gmail.com>
2022-08-24 14:41:36 +0300

    8.

--- components/About.jsx
+++ components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image src={`${process.env.NEXT_PUBLIC_ARINA}/photo.jpg`} className="rounded-xl" alt="/" />
+          <Image src={`${process.env.NEXT_PUBLIC_ARINA}/public/assets/photo.jpg`} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
